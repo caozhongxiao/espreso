f22eeabff9d68e33aa96a4450ac59439a9e8a58e
Tomas Panoc <tomas.panoc@vsb.cz>
2017-07-31 14:59:08 +0200

    FIX: [GUI] Fixed regex for decimal numbers

--- src/gui/data/common.h
+++ src/gui/data/common.h
@@ -1,7 +1,7 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#define REGEXPR_DOUBLE "^[-+]?[0-9]*.?[0-9]+([eE][-+]?[0-9]+)?$"
+#define REGEXPR_DOUBLE "^[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?$"
 #define REGEXPR_VARNAME "^[a-zA-Z_][a-zA-Z_0-9]*$"
 
 #endif // COMMON_H
