2d38b97768187beea206fc3bd7c940c9dbaacef6
Tomas Panoc <tomas.panoc@vsb.cz>
2017-08-08 10:03:25 +0200

    ENH: [GUI] Base classes for materials II

--- src/gui/data/material.cpp
+++ src/gui/data/material.cpp
@@ -1,6 +1,110 @@
 #include "material.h"
 
+MaterialProperty::MaterialProperty()
+{
+    this->mName = "";
+    this->mUnit = "";
+    this->mValue = nullptr;
+}
+
+MaterialProperty::MaterialProperty(const QString& name, const QString& unit, MaterialPropertyMatrix* const value)
+{
+    this->mName = name;
+    this->mUnit = unit;
+    this->mValue = value;
+}
+
+const QString& MaterialProperty::name() const
+{
+    return this->mName;
+}
+
+const QString& MaterialProperty::unit() const
+{
+    return this->mUnit;
+}
+
+
+MaterialPropertyMatrix* MaterialProperty::value() const
+{
+    return this->mValue;
+}
+
+
+BasicProperty::BasicProperty() : MaterialProperty() {}
+
+BasicProperty::BasicProperty(const QString& name, const QString& unit, BasicMatrix* const value) :
+    MaterialProperty(name, unit, value) {}
+
+
+IsotropicProperty::IsotropicProperty() : MaterialProperty() {}
+
+IsotropicProperty::IsotropicProperty(const QString& name, const QString& unit, IsotropicMatrix* const value) :
+    MaterialProperty(name, unit, value) {}
+
+
+DiagonalProperty::DiagonalProperty() : MaterialProperty() {}
+
+DiagonalProperty::DiagonalProperty(const QString& name, const QString& unit, DiagonalMatrix* const value) :
+    MaterialProperty(name, unit, value) {}
+
+
+SymmetricProperty::SymmetricProperty() : MaterialProperty() {}
+
+SymmetricProperty::SymmetricProperty(const QString& name, const QString& unit, SymmetricMatrix* const value) :
+    MaterialProperty(name, unit, value) {}
+
+
+AnisotropicProperty::AnisotropicProperty() : MaterialProperty() {}
+
+AnisotropicProperty::AnisotropicProperty(const QString& name, const QString& unit, AnisotropicMatrix* const value) :
+    MaterialProperty(name, unit, value) {}
+
+
 Material::Material()
 {
+    this->mName = "";
+    this->mDescription = "";
+}
 
+Material::Material(const QString& name, const QString& description)
+{
+    this->mName = name;
+    this->mDescription = description;
+}
+
+QString Material::description() const
+{
+    return this->mDescription;
+}
+
+QString Material::name() const
+{
+    return this->mName;
+}
+
+void Material::appendProperty(MaterialProperty* const property)
+{
+    this->mProperties.append(property);
+}
+
+
+void Material::removeProperty(int index)
+{
+    this->mProperties.remove(index);
+}
+
+int Material::modifyProperty(int index, MaterialProperty* const property)
+{
+    if (this->mProperties.count() <= index)
+        return -1;
+    this->mProperties.replace(index, property);
+    return 1;
+}
+
+MaterialProperty* Material::property(int index) const
+{
+    if (index >= this->mProperties.count())
+        return nullptr;
+    return this->mProperties.at(index);
 }
--- src/gui/data/material.h
+++ src/gui/data/material.h
@@ -1,11 +1,122 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
+#include <QString>
+#include <QVector>
+#include "datatype.h"
+#include "namedentity.h"
+
+
+struct MaterialPropertyMatrix
+{
+    DataType* kxx;
+};
+
+struct BasicMatrix final : MaterialPropertyMatrix
+{
+};
+
+struct IsotropicMatrix final : MaterialPropertyMatrix
+{
+};
+
+struct DiagonalMatrix final : MaterialPropertyMatrix
+{
+    DataType* kyy;
+    DataType* kzz;
+};
+
+struct SymmetricMatrix final : MaterialPropertyMatrix
+{
+    DataType* kyy;
+    DataType* kzz;
+    DataType* kxy;
+    DataType* kxz;
+    DataType* kyz;
+};
+
+struct AnisotropicMatrix final : MaterialPropertyMatrix
+{
+    DataType* kyy;
+    DataType* kzz;
+    DataType* kxy;
+    DataType* kxz;
+    DataType* kyz;
+    DataType* kyx;
+    DataType* kzx;
+    DataType* kzy;
+};
+
+
+class MaterialProperty : public NamedEntity
+{
+protected:
+    QString mName;
+    QString mUnit;
+    MaterialPropertyMatrix* mValue;
+
+    MaterialProperty();
+    MaterialProperty(const QString& name, const QString& unit, MaterialPropertyMatrix* const value);
+
+public:
+    const QString& name() const;
+    const QString& unit() const;
+    MaterialPropertyMatrix* value() const;
+};
+
+class BasicProperty final : public MaterialProperty
+{
+public:
+    BasicProperty();
+    BasicProperty(const QString& name, const QString& unit, BasicMatrix* const value);
+};
+
+class IsotropicProperty final : public MaterialProperty
+{
+public:
+    IsotropicProperty();
+    IsotropicProperty(const QString& name, const QString& unit, IsotropicMatrix* const value);
+};
+
+class DiagonalProperty final : public MaterialProperty
+{
+public:
+    DiagonalProperty();
+    DiagonalProperty(const QString& name, const QString& unit, DiagonalMatrix* const value);
+};
+
+class SymmetricProperty final : public MaterialProperty
+{
+public:
+    SymmetricProperty();
+    SymmetricProperty(const QString& name, const QString& unit, SymmetricMatrix* const value);
+};
+
+class AnisotropicProperty final : public MaterialProperty
+{
+public:
+    AnisotropicProperty();
+    AnisotropicProperty(const QString& name, const QString& unit, AnisotropicMatrix* const value);
+};
+
 
 class Material : public NamedEntity
 {
+private:
+    QString mName;
+    QString mDescription;
+    QVector<MaterialProperty*> mProperties;
+
 public:
     Material();
+    Material(const QString& name, const QString& description);
+
+    QString description() const;
+    QString name() const;
+    void appendProperty(MaterialProperty* const property);
+    void removeProperty(int index);
+    int modifyProperty(int index, MaterialProperty* const property);
+    MaterialProperty* property(int index) const;
 };
 
 #endif // MATERIAL_H
