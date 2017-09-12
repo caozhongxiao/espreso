5e2d50813df004e3e75589be0925c0a9e68b76c0
Ondrej Meca <mec059@vsb.cz>
2017-09-12 09:44:04 +0200

    RF: remove exprtk header from expression header

--- src/basis/expression/expression.cpp
+++ src/basis/expression/expression.cpp
@@ -2,6 +2,8 @@
 #include "expression.h"
 #include "../logging/logging.h"
 
+#include "exprtk.hpp"
+
 using namespace espreso;
 
 bool Expression::isValid(const std::string &str, std::vector<std::string> variables)
@@ -29,23 +31,33 @@ void Expression::parse()
 
 	_values.resize(_variables.size());
 	for (size_t i = 0; i < _variables.size(); i++) {
-		_symbol_table.add_variable(_variables[i], _values[i]);
+		_symbol_table->add_variable(_variables[i], _values[i]);
 	}
-	_symbol_table.add_constants();
-	_expression.register_symbol_table(_symbol_table);
+	_symbol_table->add_constants();
+	_expression->register_symbol_table(*_symbol_table);
 
 	exprtk::parser<double> parser;
-	parser.compile(_str, _expression);
+	parser.compile(_str, *_expression);
 }
 
 Expression::Expression(const std::string &str, std::vector<std::string> variables): _str(str), _variables(variables)
 {
+	_symbol_table = new exprtk::symbol_table<double>();
+	_expression = new exprtk::expression<double>();
 	parse();
 }
 
 Expression::Expression(const Expression &other): _str(other._str), _variables(other._variables)
 {
+	_symbol_table = new exprtk::symbol_table<double>();
+	_expression = new exprtk::expression<double>();
 	parse();
+}
+
+Expression::~Expression()
+{
+	delete _symbol_table;
+	delete _expression;
 }
 
 Expression& Expression::operator=(const Expression &other)
@@ -53,11 +65,23 @@ Expression& Expression::operator=(const Expression &other)
 	if (this != &other) {
 		_str = other._str;
 		_variables = other._variables;
+		_symbol_table = new exprtk::symbol_table<double>();
+		_expression = new exprtk::expression<double>();
 		parse();
 	}
 	return *this;
 }
 
+double Expression::operator()(const std::vector<double> &values) const
+{
+	return evaluate(values);
+}
+
+double Expression::evaluate(const std::vector<double> &values) const
+{
+	_values = values;
+	return _expression->value();
+}
 
 
 
