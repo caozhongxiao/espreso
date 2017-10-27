fd40e85de13a5ed32659e24f5971215b1e7b42f3
Ondrej Meca <mec059@vsb.cz>
2017-10-27 09:13:46 +0200

    ENH: add phase change to materials

--- src/config/ecf/material/material.cpp
+++ src/config/ecf/material/material.cpp
@@ -3,8 +3,71 @@
 
 #include "../../configuration.hpp"
 
-espreso::MaterialConfiguration::MaterialConfiguration()
-: physical_model(static_cast<PHYSICAL_MODEL>(~0)), _allowed_physical_models(static_cast<PHYSICAL_MODEL>(~0))
+using namespace espreso;
+
+MaterialBaseConfiguration::MaterialBaseConfiguration()
+: _phase_change(NULL), _physical_model(NULL)
+{
+	REGISTER(coordinate_system, ECFMetaData()
+			.setdescription({ "Coordinate system" })
+			.allowonly([&] () { return _phase_change == NULL || !*_phase_change; }));
+
+	density.value = heat_capacity.value = "0";
+	registerParameter("dens", density, ECFMetaData()
+			.setdescription({ "Density" })
+			.setdatatype({ ECFDataType::EXPRESSION })
+			.setunit("kg/m^3")
+			.setmaterialvariables()
+			.allowonly([&] () { return _phase_change == NULL || !*_phase_change; }));
+
+	registerParameter("CP", heat_capacity, ECFMetaData()
+			.setdescription({ "Heat capacity" })
+			.setdatatype({ ECFDataType::EXPRESSION })
+			.setunit("J / (kg * K)")
+			.setmaterialvariables()
+			.allowonly([&] () { return _phase_change == NULL || !*_phase_change; }));
+
+	REGISTER(thermal_conductivity, ECFMetaData()
+			.setdescription({ "Thermal conductivity" })
+			.allowonly([&] () {
+				return (_phase_change == NULL || !*_phase_change) && (*_physical_model & PHYSICAL_MODEL::THERMAL);
+			}));
+
+	REGISTER(linear_elastic_properties, ECFMetaData()
+			.setdescription({ "Linear elasticity" })
+			.allowonly([&] () { return (_phase_change == NULL || !*_phase_change) && (*_physical_model & PHYSICAL_MODEL::LINEAR_ELASTIC); }));
+}
+
+MaterialBaseConfiguration::MaterialBaseConfiguration(bool *phase_change, PHYSICAL_MODEL *physicalModel)
+: MaterialBaseConfiguration()
+{
+	_phase_change = phase_change;
+	_physical_model = physicalModel;
+}
+
+MaterialBaseConfiguration::MaterialBaseConfiguration(bool *phase_change, PHYSICAL_MODEL *physicalModel, DIMENSION *dimension)
+: MaterialBaseConfiguration(phase_change, physicalModel)
+{
+	coordinate_system.dimension = *dimension;
+	thermal_conductivity.dimension = *dimension;
+	linear_elastic_properties.dimension = *dimension;
+
+	// DROP not allowed parameters.
+	for (size_t i = 0; i < parameters.size();) {
+		if (!parameters[i]->metadata.isallowed()) {
+			dropParameter(parameters[i]);
+		} else {
+			i++;
+		}
+	}
+}
+
+MaterialConfiguration::MaterialConfiguration()
+: MaterialBaseConfiguration(&phase_change, &physical_model),
+  dimension(DIMENSION::D3),
+  physical_model(static_cast<PHYSICAL_MODEL>(~0)),
+  phase_change(false),
+  _allowed_physical_models(static_cast<PHYSICAL_MODEL>(~0))
 {
 	name = "MATERIAL_NAME";
 	REGISTER(name, ECFMetaData()
@@ -16,21 +79,7 @@ espreso::MaterialConfiguration::MaterialConfiguration()
 			 .setdescription({ "Description" })
 			 .setdatatype( { ECFDataType::STRING } ));
 
-	REGISTER(coordinate_system, ECFMetaData()
-			.setdescription({ "Coordinate system" }));
-
-	density.value = heat_capacity.value = "0";
-	registerParameter("dens", density, ECFMetaData()
-			.setdescription({ "Density" })
-			.setdatatype({ ECFDataType::EXPRESSION })
-			.setunit("kg/m^3")
-			.setmaterialvariables());
-
-	registerParameter("CP", heat_capacity, ECFMetaData()
-			.setdescription({ "Heat capacity" })
-			.setdatatype({ ECFDataType::EXPRESSION })
-			.setunit("J / (kg * K)")
-			.setmaterialvariables());
+	addSpace();
 
 	REGISTER(physical_model, ECFMetaData()
 			.setdescription({ "Physical model" })
@@ -40,17 +89,49 @@ espreso::MaterialConfiguration::MaterialConfiguration()
 			.addoption(ECFOption().setname("LINEAR_ELASTIC").setdescription("One of models used by STRUCTURAL MECHANICS.")
 					.allowonly([&] () { return _allowed_physical_models & PHYSICAL_MODEL::LINEAR_ELASTIC; })));
 
-	REGISTER(thermal_conductivity, ECFMetaData()
-			.setdescription({ "Thermal conductivity" })
-			.allowonly([&] () {  return physical_model & PHYSICAL_MODEL::THERMAL; }));
+	REGISTER(phase_change, ECFMetaData()
+			.setdescription({ "Turn on/off phase change." })
+			.setdatatype({ ECFDataType::BOOL }));
 
-	REGISTER(linear_elastic_properties, ECFMetaData()
-			.setdescription({ "Linear elasticity" })
-			.allowonly([&] () {  return physical_model & PHYSICAL_MODEL::LINEAR_ELASTIC; }));
+	addSeparator();
+
+	moveLastBefore(parameters.front()->name);
+	moveLastBefore(parameters.front()->name);
+	moveLastBefore(parameters.front()->name);
+	moveLastBefore(parameters.front()->name);
+	moveLastBefore(parameters.front()->name);
+	moveLastBefore(parameters.front()->name);
+
+	smooth_step_order = 1;
+	REGISTER(smooth_step_order, ECFMetaData()
+			.setdescription({ "Smooth step order." })
+			.setdatatype({ ECFDataType::NONNEGATIVE_INTEGER })
+			.allowonly([&] () { return phase_change; }));
+
+	latent_heat = transition_interval = phase_change_temperature = 0;
+	REGISTER(latent_heat, ECFMetaData()
+			.setdescription({ "Latent heat." })
+			.setdatatype({ ECFDataType::FLOAT })
+			.allowonly([&] () { return phase_change; }));
+	REGISTER(transition_interval, ECFMetaData()
+				.setdescription({ "Transition interval." })
+				.setdatatype({ ECFDataType::FLOAT })
+				.allowonly([&] () { return phase_change; }));
+	REGISTER(phase_change_temperature, ECFMetaData()
+				.setdescription({ "Phase change temperature." })
+				.setdatatype({ ECFDataType::FLOAT })
+				.allowonly([&] () { return phase_change; }));
+
+	REGISTER(phases, ECFMetaData()
+			.setdescription({ "Phase change settings.", "Settings for a particular phase." })
+			.setdatatype({ ECFDataType::POSITIVE_INTEGER })
+			.setpattern({ "1" })
+			.allowonly([&] () { return phase_change; }),
+			static_cast<bool*>(NULL), &physical_model, &dimension);
 }
 
-espreso::MaterialConfiguration::MaterialConfiguration(DIMENSION dimension, PHYSICAL_MODEL allowedPhysicalModels)
-: espreso::MaterialConfiguration()
+MaterialConfiguration::MaterialConfiguration(DIMENSION dimension, PHYSICAL_MODEL allowedPhysicalModels)
+: MaterialConfiguration()
 {
 	_allowed_physical_models = allowedPhysicalModels;
 	coordinate_system.dimension = dimension;
@@ -61,11 +142,18 @@ espreso::MaterialConfiguration::MaterialConfiguration(DIMENSION dimension, PHYSI
 	// GUI can edit materials for all physics.
 	// Default physics should print only subset of parameters.
 
-	// DROP not allowed parameters.
+	// DROP not allowed models
 	physical_model = allowedPhysicalModels;
+	bool phaseChange = phase_change;
 	for (size_t i = 0; i < parameters.size();) {
 		if (!parameters[i]->metadata.isallowed()) {
-			dropParameter(parameters[i]);
+			phase_change = !phase_change;
+			if (!parameters[i]->metadata.isallowed()) {
+				dropParameter(parameters[i]);
+			} else {
+				i++;
+			}
+			phase_change = phaseChange;
 		} else {
 			i++;
 		}
@@ -78,4 +166,21 @@ espreso::MaterialConfiguration::MaterialConfiguration(DIMENSION dimension, PHYSI
 			break;
 		}
 	}
+}
+
+MaterialConfiguration& MaterialConfiguration::operator=(const MaterialConfiguration &other)
+{
+	if (this != &other) {
+		name = other.name;
+		description = other.description;
+		physical_model = other.physical_model;
+		_allowed_physical_models = other._allowed_physical_models;
+
+		*dynamic_cast<MaterialBaseConfiguration*>(this) = dynamic_cast<const MaterialBaseConfiguration&>(other);
+
+		for (auto it = other.phases.begin(); it != other.phases.end(); ++it) {
+			phases[it->first] = it->second;
+		}
+	}
+	return *this;
 }
--- src/config/ecf/material/material.h
+++ src/config/ecf/material/material.h
@@ -8,27 +8,48 @@
 
 namespace espreso {
 
-struct MaterialConfiguration: public ECFObject {
+struct MaterialBaseConfiguration: public ECFObject {
 
 	enum PHYSICAL_MODEL {
 		THERMAL        = 1 << 0,
 		LINEAR_ELASTIC = 1 << 1
 	};
 
-	std::string name;
-	std::string description;
-
 	CoordinateSystemConfiguration coordinate_system;
-
-	PHYSICAL_MODEL physical_model;
 
 	ECFExpression density;
 	ECFExpression heat_capacity;
 	LinearElasticPropertiesConfiguration linear_elastic_properties;
 	ThermalConductivityConfiguration thermal_conductivity;
 
+	MaterialBaseConfiguration();
+	MaterialBaseConfiguration(bool *phase_change, PHYSICAL_MODEL *physicalModel);
+	MaterialBaseConfiguration(bool *phase_change, PHYSICAL_MODEL *physicalModel, DIMENSION *dimension);
+
+protected:
+	bool *_phase_change;
+	PHYSICAL_MODEL *_physical_model;
+};
+
+struct MaterialConfiguration: public MaterialBaseConfiguration {
+
+	std::string name;
+	std::string description;
+
+	DIMENSION dimension;
+	PHYSICAL_MODEL physical_model;
+
+	bool phase_change;
+	size_t smooth_step_order;
+	double latent_heat, transition_interval, phase_change_temperature;
+
+	std::map<size_t, MaterialBaseConfiguration> phases;
+
 	MaterialConfiguration();
 	MaterialConfiguration(DIMENSION dimension, PHYSICAL_MODEL allowedPhysicalModels);
+
+	// allow assign operator (needed because phases is std::map of ECFOBjects)
+	MaterialConfiguration& operator=(const MaterialConfiguration &other);
 
 protected:
 	PHYSICAL_MODEL _allowed_physical_models;
