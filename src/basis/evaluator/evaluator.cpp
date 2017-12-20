a2d42575b7acfd6b0b41f0c256269c61a992d80b
Ondrej Meca <mec059@vsb.cz>
2017-12-20 10:36:02 +0100

    FIX: evaluator

--- src/basis/evaluator/evaluator.cpp
+++ src/basis/evaluator/evaluator.cpp
@@ -4,7 +4,7 @@
 using namespace espreso;
 
 
-double Evaluator::evaluate(const Point &p, double temperature, double time) const
+double Evaluator::evaluate(const Point &p, double time, double temperature) const
 {
 	double result;
 	evaluate(1, &p, &temperature, time, &result);
