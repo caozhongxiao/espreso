3bbad29e369bc0c0c018b39c3e87f37d30fdad2a
Ondrej Meca <mec059@vsb.cz>
2018-02-09 15:25:38 +0100

    ENH: improve testing

--- tests/benchmarks.py
+++ tests/benchmarks.py
@@ -14,25 +14,38 @@ class ESPRESOBenchmarks(unittest.TestCase):
 
     espreso = Espreso()
 
-    def benchmark(self, path, file):
+    def benchmark(self, path, file, args, report):
         self.espreso.run(
             self.espreso.get_processes(os.path.join(path, file)),
             path,
-            { "config": os.path.join(path, file), "ENV::VERBOSE_LEVEL": 0, "ENV::MEASURE_LEVEL": 0, "OUTPUT::RESULTS_STORE_FREQUENCY": "NEVER" }
+            { "config": os.path.join(path, file), "ENV::VERBOSE_LEVEL": 0, "ENV::MEASURE_LEVEL": 0, "OUTPUT::RESULTS_STORE_FREQUENCY": "NEVER" },
+            args.values()
         )
 
-#        self.espreso.compare_monitors(
-#            os.path.join(path, file.replace(".ecf", ".emr")),
-#            os.path.join(path, "results", "last", file.replace(".ecf", ".emr"))
-#        )
+        self.espreso.compare_monitors(
+            os.path.join(path, report),
+            os.path.join(path, "results", "last", file.replace(".ecf", ".emr"))
+        )
 
+        shutil.rmtree(os.path.join(path, "results"))
 
 if __name__ == '__main__':
 
     benchmarks = TestCaseCreator.select(os.path.join(ROOT, "benchmarks"))
 
+    def run(vars, range, name, path, file):
+        def call(args):
+            report = ""
+            for var in vars:
+                report += args[var] + "."
+            report = ".".join([ args[var] for var in vars ])
+            TestCaseCreator.create_test(ESPRESOBenchmarks, ESPRESOBenchmarks.benchmark, name + "." + report, path, file, args, report + ".emr")
+
+        TestCaseCreator.iterate(call, range)
+
     for subdirectory in benchmarks:
         for name, path, file in TestCaseCreator.gather(subdirectory, ".ecf"):
-            TestCaseCreator.create_test(ESPRESOBenchmarks, ESPRESOBenchmarks.benchmark, name, path, file)
+            vars, range = Espreso.get_instances(os.path.join(path, file))
+            run(vars, range, name, path, file)
 
     unittest.main()
