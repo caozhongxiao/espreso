1e8e870bca3e63c0513f3ec2449ec4cf2cd5cd09
Ondrej Meca <mec059@vsb.cz>
2018-02-14 07:55:47 +0100

    ENH: add all signal handlers

--- src/app/factory/factory.cpp
+++ src/app/factory/factory.cpp
@@ -27,11 +27,23 @@ namespace espreso {
 static void signalHandler(int signal)
 {
 	switch (signal) {
+	case SIGTERM:
+		ESINFO(ERROR) << "SIGTERM -termination request, sent to the program";
+		break;
 	case SIGSEGV:
-		ESINFO(ERROR) << "Invalid memory reference";
+		ESINFO(ERROR) << "SIGSEGV - invalid memory access (segmentation fault)";
+		break;
+	case SIGINT:
+		ESINFO(ERROR) << "SIGINT - external interrupt, usually initiated by the user";
+		break;
+	case SIGILL:
+		ESINFO(ERROR) << "SIGILL - invalid program image, such as invalid instruction";
+		break;
+	case SIGABRT:
+		ESINFO(ERROR) << "SIGABRT - abnormal termination condition, as is e.g. initiated by std::abort()";
 		break;
 	case SIGFPE:
-		ESINFO(ERROR) << "Erroneous arithmetic operation";
+		ESINFO(ERROR) << "SIGFPE - erroneous arithmetic operation such as divide by zero";
 		break;
 	default:
 		ESINFO(ERROR) << "ESPRESO trigger error " << signal << ".";
@@ -40,8 +52,12 @@ static void signalHandler(int signal)
 
 void ESPRESO::run(int *argc, char ***argv)
 {
-	std::signal(SIGFPE, signalHandler);
+	std::signal(SIGTERM, signalHandler);
 	std::signal(SIGSEGV, signalHandler);
+	std::signal(SIGINT, signalHandler);
+	std::signal(SIGILL, signalHandler);
+	std::signal(SIGABRT, signalHandler);
+	std::signal(SIGFPE, signalHandler);
 
 	ECFRoot configuration(argc, argv);
 	Mesh mesh(configuration);
