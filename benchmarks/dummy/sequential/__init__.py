6b128f809f0a21aaff2514120b1c24c8aeb44525
Ondrej Meca <ondrej.meca@vsb.cz>
2018-10-25 15:37:33 +0200

    FIX: threading during testing

--- benchmarks/dummy/sequential/__init__.py
+++ benchmarks/dummy/sequential/__init__.py
@@ -7,3 +7,6 @@ def setup():
     ESPRESOTest.env["OMP_NUM_THREADS"] = "1"
     ESPRESOTest.env["SOLVER_NUM_THREADS"] = "1"
     ESPRESOTest.env["PAR_NUM_THREADS"] = "1"
+
+def teardown():
+    ESPRESOTest.mpirun = [ "mpirun", "--map-by", "socket:pe=2", "--bind-to", "core", "-n" ]
